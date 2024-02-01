1238560986736f0a52850da42843625ace69d690
이혜린 <hyerin@ihyelin-ui-noteubug.local>
2024-02-01 23:10:59 +0900

    카드 정렬하기 / 30m

--- 정렬/카드_정렬하기/hyxrxn.cpp
+++ 정렬/카드_정렬하기/hyxrxn.cpp
@@ -1,3 +1,31 @@
-//
-// Created by 이혜린 on 2/1/24.
-//
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n;
+int number = 0;
+priority_queue<int, vector<int>, greater<int> > pq;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int card;
+        cin >> card;
+        pq.push(card);
+    }
+
+    while (pq.size() > 1) {
+        int small = pq.top();
+        pq.pop();
+        int next = pq.top();
+        pq.pop();
+        int dummy = small + next;
+        number += dummy;
+        pq.push(dummy);
+    }
+
+    cout << number;
+
+    return 0;
+}
