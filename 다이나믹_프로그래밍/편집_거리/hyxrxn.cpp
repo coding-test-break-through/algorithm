682a2d5bf80de41c7a03d611ff76236b92fecc3a
이혜린 <hyerin@ihyelin-ui-noteubug.local>
2024-02-10 03:04:40 +0900

    편집 거리 / 58m(실패)

---
+++ 다이나믹_프로그래밍/편집_거리/hyxrxn.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+string a, b;
+
+int main() {
+    cin >> a >> b;
+    int i = 0;
+    int j = 0;
+    int skip = 0;
+    int answer = 0;
+
+    while (i < a.size() && j < b.size()) {
+
+    }
+
+
+    return 0;
+}
+
+// 2:05~3:03
